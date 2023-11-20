dc98c62d0d37829c9955f217b1b7404a0b6853ff
Filippo Toso <filippo.toso@creativepark.it>
2023-11-20 13:29:59 +0100

    Add support for connections

--- src/FullText.php
+++ src/FullText.php
@@ -6,7 +6,8 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use InvalidArgumentException;
 
-class FullText {
+class FullText
+{
 
     /**
      * Add a FULLTEXT index to the specified table
@@ -14,25 +15,25 @@ class FullText {
      * @param  string        $table   Name of the database table
      * @param  string|array  $columns Columns to be included in the index
      * @param  string|null   $name    Name of the index (optional)
+     * @param  string|null   $connection    Name of the connection to be used (optional)
      * @return void
      */
-    public static function fullTextIndex($table, $columns, $name = null) {
+    public static function fullTextIndex($table, $columns, $name = null, $connection = null)
+    {
 
         $columns = is_array($columns) ? $columns : [$columns];
 
         $name = is_null($name) ? 'ftidx_' . implode('_', $columns) : $name;
 
-        if (!Schema::hasTable($table)) {
+        if (!Schema::connection($connection)->hasTable($table)) {
             throw new InvalidArgumentException('The specified table can\'t be found: ' . $table);
         }
 
-        if (!Schema::hasColumns($table, $columns)) {
+        if (!Schema::connection($connection)->hasColumns($table, $columns)) {
             throw new InvalidArgumentException('One or more specified columns can\t be found in the table: ' . $table);
         }
 
         $sql = sprintf('CREATE FULLTEXT INDEX `%s` ON `%s` (`%s`)', $name, $table, implode('`, `', $columns));
-        DB::statement($sql);
-
+        DB::connection($connection)->statement($sql);
     }
-
 }
--- src/Traits/FullText.php
+++ src/Traits/FullText.php
@@ -3,8 +3,6 @@
 namespace FilippoToso\FullText\Traits;
 
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
-use InvalidArgumentException;
 
 trait FullText
 {
@@ -15,7 +13,8 @@ trait FullText
      * @param  string     $identifier The name of the identifier
      * @return string     Quoted identifier
      */
-    protected static function quoteIdentifier($identifier) {
+    protected static function quoteIdentifier($identifier)
+    {
         return '`' . str_replace('`', '``', $identifier) . '`';
     }
 
@@ -27,18 +26,18 @@ trait FullText
      * @param  string        $text    Text to execute the search against
      * @return QueryBuilder
      */
-    public function scopeWhereMatch($query, $columns, $text) {
+    public function scopeWhereMatch($query, $columns, $text)
+    {
 
         $columns = is_array($columns) ? $columns : [$columns];
 
-        $columns = array_map(function($column){
+        $columns = array_map(function ($column) {
             return static::quoteIdentifier($column);
         }, $columns);
 
         $sql = sprintf('MATCH (%s) AGAINST (? IN NATURAL LANGUAGE MODE)', implode(',', $columns));
 
         return $query->whereRaw($sql, [$text]);
-
     }
 
     /**
@@ -50,22 +49,20 @@ trait FullText
      * @param  string|null   $name    Name of the field (optional, default: score)
      * @return QueryBuilder
      */
-    public function scopeSelectMatchScore($query, $columns, $text, $name = 'score') {
-
+    public function scopeSelectMatchScore($query, $columns, $text, $name = 'score')
+    {
         $columns = is_array($columns) ? $columns : [$columns];
 
-        $columns = array_map(function($column){
+        $columns = array_map(function ($column) {
             return static::quoteIdentifier($column);
         }, $columns);
 
         $name = static::quoteIdentifier($name);
 
-        $text = DB::connection()->getPdo()->quote($text);
+        $text = DB::connection($this->getConnectionName())->getPdo()->quote($text);
 
         $sql = sprintf('MATCH (%s) AGAINST (%s IN NATURAL LANGUAGE MODE) AS %s', implode(',', $columns), $text, $name);
 
         return $query->addSelect(DB::raw($sql));
-
     }
-
 }
